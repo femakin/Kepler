4867607937c2e15dde8220cb381ba0252a75b6e1
femi <oluwafemi.akinyemi@datamellon.com>
2024-01-02 16:01:10 +0100

    femi-akinyemi-work

--- src/problem1-lis.ts
+++ src/problem1-lis.ts
@@ -1,8 +1,35 @@
+
+
+
 export function getLongestIncreasingSubsequence(arr: number[]): number {
   const n: number = arr.length;
   const subSequence: number[] = new Array(n).fill(1);
+  let result = []
+  for (let i: number = 1; i < n; i++) {
+    if (n[2] === n[i + 2]) {
+      // result.push(n] )
+      console.log(n[i], 'iiiii')
+    }
 
-  for (let i: number = 1; i < n; i++) {}
+  }
 
   return 0;
 }
+
+// nums = [10, 9, 2, 5, 3, 7, 101, 18]
+
+// A subsequence is a sequence of numbers in an array that are in the same order as they appear in the array,
+//  but not necessarily contiguous.The numbers in the subsequence must be strictly increasing.
+
+
+
+// Example 1:
+// Input: nums = [10, 9, 2, 5, 3, 7, 101, 18] Output: 4 Explanation: The longest increasing subsequence is[2, 3, 7, 101], therefore the length is 4.
+
+// nums = [10, 9, 2, 5, 3, 7, 101, 18]
+
+// The longest increasing subsequence is [2, 3, 7, 101]
+
+// [2, 3, 7, 18, 101]
+
+// Output = []
--- src/problem2-groupby.ts
+++ src/problem2-groupby.ts
@@ -5,5 +5,37 @@ type GroupedData<T> = Record<KeyType, T[]>;
 export function groupBy<T>(arr: T[], key: keyof T): GroupedData<T> {
   const grouped: GroupedData<T> = {};
 
+  // Given data Types = Array of Objects
+  // Given data = Array (Input)
+  // To Do
+  // To group by key
+  // check the key/value I'm grouping by
+
+  for (let i = 0; i < arr.length; i++) {
+
+    // console.log(arr[i], 'This is the log')
+
+    // if () {
+
+    // }
+    console.log([], 'individual Object')
+
+    // console.log([...arr[i]], 'first sample')
+
+
+
+  }
+
+  // for (const key in arr) {
+  //   // if (Object.prototype.hasOwnProperty.call(object, key)) {
+  //   //   const element = object[key];
+
+  //   // }
+
+  //   console.log(grouped, 'first sample')
+
+  // }
+
+
   return grouped;
 }
